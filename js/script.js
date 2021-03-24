98f00c64e4ffcb6a79a4c55f118712f1107e3510
DonLe0pardo <christinatemnikova@gmail.com>
2021-03-25 01:29:27 +0500

    comit 3

--- js/script.js
+++ js/script.js
@@ -5,3 +5,22 @@ burger.addEventListener('click', () => {
     menu.classList.toggle('header-mobile_active');
     burger.classList.toggle('burger_active');
 });
+
+
+
+
+
+document.querySelector('.skills__stars-items').onclick = () => {
+    let rating = document.querySelectorAll('.skills__stars-input');
+    for (let i = 0; i < rating.length; i++) {
+        if (rating[i].checked) {
+            let result = rating[i].value;
+            localStorage.setItem("myKey", JSON.stringify(result));
+            break;
+        }
+    }
+     localStorage.getItem("myKey");
+      
+  };
+    
+ 
